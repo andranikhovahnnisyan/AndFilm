16e7d42c781ed4a73e4d105cd86590f76b33c735
Andranik <andranikhovahnnisyan206@gmail.com>
2026-07-26 22:34:36 +0400

    Update script.js

--- AndFilm/script.js
+++ AndFilm/script.js
@@ -11,8 +11,9 @@ document.documentElement.style.setProperty('--header_footer-color', '#fff');
 document.documentElement.style.setProperty('--white', '#000');
 document.documentElement.style.setProperty('--hover-card', '#000');
 document.documentElement.style.setProperty('--white-hover', 'rgba(0, 0, 0, 0.278)');
-main.style.backgroundColor = "#ededed";
-search.style.backgroundColor = "#e2e2e2";
+document.documentElement.style.setProperty('--ftr-svg-hover', '#313131');
+main.style.backgroundColor = "#dcdedffd";
+search.style.backgroundColor = "#f1f3f4";
 })
 
 themeMoonBtn.addEventListener('click', () => {
@@ -22,6 +23,7 @@ document.documentElement.style.setProperty('--header_footer-color', '#242227');
 document.documentElement.style.setProperty('--white', '#fff');
 document.documentElement.style.setProperty('--hover-card', 'rgb(147, 146, 146)');
 document.documentElement.style.setProperty('--white-hover', 'rgba(255, 255, 255, 0.278)');
+document.documentElement.style.setProperty('--ftr-svg-hover', '#e9e9e9');
 main.style.backgroundColor = "#151617";
 search.style.backgroundColor = "rgba(63, 61, 67, 0.956)";
 })
